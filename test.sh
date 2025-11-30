bc561144b11696a5e649ccca81ec72094ed6d110
aussie114 <redacted@example.com>
2025-12-01 08:40:00 +1000

    modified config file

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-./sticky_notes 0 "3/Dec/25 water bill" "7/Dec/2025 power bill" "10/Dec/2025 phone bill" & disown
-./sticky_notes "#ce81ff" "heather's birthday" & disown
-./sticky_notes "#71D4FF" "call david back" & disown
-./sticky_notes "#FF7ECD" "interview 10:00am" & disown
-./sticky_notes "#cdfc93" "make stick notes video" & disown
+bin/sticky_notes 0 "3/Dec/25 water bill" "7/Dec/2025 power bill" "10/Dec/2025 phone bill" & disown
+bin/sticky_notes "#ce81ff" "heather's birthday" & disown
+bin/sticky_notes "#71D4FF" "call david back" & disown
+bin/sticky_notes "#FF7ECD" "interview 10:00am" & disown
+bin/sticky_notes "#cdfc93" "make stick notes video" & disown
